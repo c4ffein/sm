17fa1e94867e6bc8403bed683d78fb097d6ab703
c4ffein <c4ffein@gmail.com>
2025-04-07 23:53:00 +0200

    just name

--- sm.py
+++ sm.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-sm - KISS cli to send email
+sm - Simple Mail client
 MIT License - Copyright (c) 2025 c4ffein
 WARNING: I don't recommand using this as-is. This a PoC, and usable by me because I know what I want to do with it.
 - You can use it if you feel that you can edit the code yourself and you can live with my future breaking changes.
